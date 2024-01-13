d6dca0d9d0f74234286304e0d12e7cecd30e1e31
CyberProger <booleansandwich@gmail.com>
2024-01-14 00:25:24 +0200

    started next proj

---
+++ travel-list/src/App.js
@@ -0,0 +1,28 @@
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your trip? 😍</h3>
+  </div>
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return <footer className="stats">
+    <em> 👜 You have X items on your list, and you already packed X(X%)</em>
+  </footer>
+}
