f0326c7a8be90d7989f0cb770694c5e2d9013b6c
CyberProger <booleansandwich@gmail.com>
2024-01-14 03:02:43 +0200

    style and state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+
 export default function App() {
   return <div className="app">
     <Logo />
@@ -12,13 +20,48 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(5);
+  function handleSumbit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem);
+    setDescription('')
+    setQuantity('')
+
+  }
+  return <form className="add-form" onSubmit={handleSumbit}>
     <h3>What do you need for your trip? 😍</h3>
-  </div>
+    <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
+      {Array.from({ length: 20 }, (_, i) => i + 1)
+        .map(num => <option value={num} key={num}>
+          {num}
+        </option>)}
+    </select>
+    <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)} />
+    <button>Add</button>
+  </form>
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>
+  return (
+    <div className="list">
+      <ul >
+        {initialItems.map(item => <Item item={item} key={item.id} />)}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return <li>
+    <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+      {item.description} {item.quantity}
+    </span>
+    <button>❌</button>
+  </li>
 }
 
 function Stats() {
